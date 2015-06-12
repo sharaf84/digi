ad474ebb8ed7481b23f14ba1a0504024ce610496
Islam Magdy <i.magdy.m@gmail.com>
2015-06-12 14:46:09 +0200

    saving up single blog and blog listing

--- frontend/views/site/contact.php
+++ frontend/views/site/contact.php
@@ -1,37 +1,53 @@
 <?php
-use yii\helpers\Html;
-use yii\bootstrap\ActiveForm;
-use yii\captcha\Captcha;
-
-/* @var $this yii\web\View */
-/* @var $form yii\bootstrap\ActiveForm */
-/* @var $model \frontend\models\ContactForm */
-
-$this->title = 'Contact';
-$this->params['breadcrumbs'][] = $this->title;
+	use yii\helpers\Html;
+	use yii\widgets\ActiveForm;
+	use yii\captcha\Captcha;
+	$this->title = 'Contact';
+	$this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-contact">
-    <h1><?= Html::encode($this->title) ?></h1>
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
 
-    <p>
-        If you have business inquiries or other questions, please fill out the following form to contact us. Thank you.
-    </p>
+<div id="checkpoint-a" class="single-page contact-us-page row">
+	
+	<div class="page-title large-12 medium-12 small-12 columns">
+		<h2>Contact Us</h2>
+	</div>
+	
+	<div class="row">
+		<div class="large-6 medium-6 small-12 columns company-meta">
+			<p>
+				<strong>Address:</strong>
+				15 AlFustat Street, 2nd Settelment, Fustat City, Masr Al Qadeema, Cairo, Egypt.
+			</p>
+			<p>
+				<strong>Telephone:</strong>
+				02-27429783 - 02-27429783, 19402
+			</p>
+			<p>
+				<strong>Fax:</strong>
+				02-27424323
+			</p>
+		</div>
+		<div class="large-6 medium-6 small-12 columns">
+			<?php $form = ActiveForm::begin(['id' => 'contact-form']); ?>
+				<?= $form->field($model, 'name')->textInput(['placeholder' => 'Your Name'])->label(false) ?>
+				<?= $form->field($model, 'email')->textInput(['placeholder' => 'Email Address'])->label(false) ?>
+				<?= $form->field($model, 'subject')->textInput(['placeholder' => 'Subject'])->label(false) ?>
+				<?= $form->field($model, 'body')->textArea(['rows' => 6, 'placeholder' => 'Message'])->label(false) ?>
+				<?= Html::submitButton('Submit', ['class' => 'right', 'name' => 'contact-button']) ?>
+			<?php ActiveForm::end(); ?>
+		</div>
+	</div>
+	
+	<div class="row">
+		<div class="page-title large-12 medium-12 small-12 columns">
+			<h2>Store Locator</h2>
+		</div>
+	</div>
+	
+	
+</div>
 
-    <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'contact-form']); ?>
-                <?= $form->field($model, 'name') ?>
-                <?= $form->field($model, 'email') ?>
-                <?= $form->field($model, 'subject') ?>
-                <?= $form->field($model, 'body')->textArea(['rows' => 6]) ?>
-                <?= $form->field($model, 'verifyCode')->widget(Captcha::className(), [
-                    'template' => '<div class="row"><div class="col-lg-3">{image}</div><div class="col-lg-6">{input}</div></div>',
-                ]) ?>
-                <div class="form-group">
-                    <?= Html::submitButton('Submit', ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
-                </div>
-            <?php ActiveForm::end(); ?>
-        </div>
-    </div>
-
+<div class="contact-us-map-cont">
+	<div id="contact-us-map"></div>
 </div>
