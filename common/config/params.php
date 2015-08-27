63e2c49a6365f3d4dbd5aa65853c6fb223c00b32
Ahmed Sharaf <a.sharaf@digitreeinc.com>
2015-08-27 14:26:45 +0200

    One logged user throw subdomains & profile & admin sidebar

--- common/config/params.php
+++ common/config/params.php
@@ -4,15 +4,4 @@ return [
     'supportEmail' => 'support@example.com',
     'user.passwordResetTokenExpire' => 259200, // 72 hours
     'user.verificationTokenExpire' => 259200, // 72 hours
-    'mlConfig'=>[
-        'default_language'=>'en',
-        'languages'=>[
-            'en'=>'English',
-            'ar'=>'Arabic',
-        ],
-        'subdomains' => [
-            'en' => 'http://local.tss.com', 
-            'ar' => 'http://ar.tss.com',
-        ]
-    ],
 ];
